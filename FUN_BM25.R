3684516789437a6420821d7a3458aa4e3560bebe
CharleneMSI <q2330416@gmail.com>
2022-01-11 19:32:32 +0800

    Modify the document length

--- FUN_BM25.R
+++ FUN_BM25.R
@@ -20,26 +20,45 @@
 ##### (FUN) BM25Score #####
   BM25Score = function(tbl, term, document, n, 
                        b = 0.75, k1 = 1.25, sigmoid=1){
+    ## Environment
+    require(dplyr)
+    require(magrittr)
+    require(tidyverse)
     
+    ##
     term <- quo_name(enquo(term))
     document <- quo_name(enquo(document))
     n_col <- quo_name(enquo(n))
     terms <- as.character(tbl[[term]])
     documents <- as.character(tbl[[document]])
     
-    doc_len <- length(documents)
-    mean_doc_len <- mean(vapply(documents, length, FUN.VALUE = integer(1)))
+
     
+  
     n <- tbl[[n_col]]
     doc_totals <- tapply(n, documents, sum)
+    
+    # doc_len <- length(documents)
+    # mean_doc_len <- mean(vapply(documents, length, FUN.VALUE = integer(1)))
+    tbl %>% group_by(book) %>% mutate(.,Ld=sum(n)) -> doc_len
+    #doc_len <- doc_len[!duplicated(doc_len[,c('book')]),]
+    #book_BM25 %>% group_by(book) %>% mutate(.,Ld=sum(n)) -> doc_len
+    doc_len <- doc_len$Ld
+    names(doc_len) <- terms
+    mean_doc_len <- sum(tbl$n)/length(doc_totals)
+    
     idf <- log(length(doc_totals)/table(terms))
     idf_BM25 <- log((length(doc_totals)-table(terms)+0.5)/(table(terms)+0.5))
     tbl$tf <- n/as.numeric(doc_totals[documents])
     tbl$idf <- as.numeric(idf[terms])
     tbl$idf_BM25 <- as.numeric(idf_BM25[terms])
     tbl$tf_idf <- tbl$tf * tbl$idf
+    tbl$doc_len <- as.numeric(doc_len[terms])
+    
+
+    
     tbl$bm25 <- (k1 + 1)*(tbl$tf) * tbl$idf_BM25/
-      (k1*(1 - b + b *(doc_len / mean_doc_len))+ tbl$tf)
+      (k1*(1 - b + b *(tbl$doc_len / mean_doc_len))+ tbl$tf)
     if (sigmoid==1){
       ## Sigmoid
       # https://cran.r-project.org/web/packages/sigmoid/vignettes/sigmoid.html
@@ -55,8 +74,8 @@
     return(tbl)    
   }
   
-##### Try #####
-  # book_BM25 <- book_words %>%
-  #   BM25Score(word, book, n)
-  # 
-  # plot(book_BM25_sig$tf_idf ,book_BM25_sig$bm25)
+# ##### Try #####
+#   book_BM25 <- book_words %>%
+#     BM25Score(word, book, n)
+# 
+#   plot(book_BM25$tf_idf ,book_BM25$bm25)
