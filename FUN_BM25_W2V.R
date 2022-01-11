5793374504781a03438ed639e7a7d2d111192a5c
CharleneMSI <q2330416@gmail.com>
2022-01-11 23:37:24 +0800

    Modify the Score

--- FUN_BM25_W2V.R
+++ FUN_BM25_W2V.R
@@ -23,16 +23,20 @@
 #   W2V <- sample(1:50)*0.01
 
 ##### (FUN) BM25 combine W2V Score #####
-  BM25_W2V_Score = function(tbl, BM25, W2V, document, mode=1, b = 0.75){
+  BM25_W2V_Score = function(tbl, term, BM25, W2V, document, n, 
+                            mode=1, b = 0.75, sigmoid=2, MinMax=1){
     
+      term <- quo_name(enquo(term))
       BM25 <- quo_name(enquo(BM25))
       W2V <- quo_name(enquo(W2V))
       document <- quo_name(enquo(document))
+      n_col <- quo_name(enquo(n))
       
-      BM25s <-  as.numeric(tbl[[document]])
-      W2Vs  <-  as.numeric(tbl[[document]])
+      terms <- as.character(tbl[[term]])
+      BM25s <-  as.numeric(tbl[[BM25]])
+      W2Vs  <-  as.numeric(tbl[[W2V]])
       documents <- as.character(tbl[[document]]) 
-      
+      n <- tbl[[n_col]]
       
       ## https://rdrr.io/cran/superml/src/R/bm25.R
       ## Not suitable
@@ -43,22 +47,38 @@
       tbl %>% group_by(book) %>% mutate(.,Ld=sum(n)) -> doc_len
       #doc_len <- doc_len[!duplicated(doc_len[,c('book')]),]
       #book_BM25 %>% group_by(book) %>% mutate(.,Ld=sum(n)) -> doc_len
-      doc_len <- doc_len$Ld
+      doc_len <- as.numeric(doc_len$Ld)
+      names(doc_len) <- terms
+      doc_totals <- tapply(n, documents, sum)
       mean_doc_len <- sum(tbl$n)/length(doc_totals)
-      
+      tbl$doc_len <- as.numeric(doc_len[terms])
       
       
       if (mode==1){
-      Score <- sum(BM25s*W2Vs)/(1 - b + b *(doc_len / mean_doc_len))
+      Score <- (BM25s*W2Vs)/(1 - b + b *(tbl$doc_len / mean_doc_len))
       }else{
-      Score <- sum(BM25s+W2Vs)/(1 - b + b *(doc_len / mean_doc_len))  
+      Score <- (BM25s+W2Vs)/(1 - b + b *(tbl$doc_len / mean_doc_len))  
+      }
+      
+      if (sigmoid==1){
+        ## Sigmoid
+        # https://cran.r-project.org/web/packages/sigmoid/vignettes/sigmoid.html
+        library(sigmoid)
+        Score <- sigmoid(Score,method = c("logistic"))
+      }
+      
+      if (MinMax==1){
+        MinMaxFun <- function(x) {
+          (x - min(x)) / (max(x) - min(x))
+        }
+        Score <- MinMaxFun(Score)
       }
       tbl$Score <- Score
       return(tbl)    
     }
 
-# ##### Try #####
-  BM25_W2V_Score.df <- BM25_W2V_Score(book_BM25, tf_idf, bm25, book)
+# # ##### Try #####
+#   BM25_W2V_Score.df <- BM25_W2V_Score(book_BM25, tf_idf, bm25, book)
 
 
 
