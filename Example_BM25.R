a18d07633a47f4ec6d805a469b6d66431815aab2
CharleneMSI <q2330416@gmail.com>
2022-01-11 15:58:06 +0800

    Move to function script

--- Example_BM25.R
+++ Example_BM25.R
@@ -2,7 +2,22 @@
 # https://www.tidytextmining.com/tfidf.html
   rm(list = ls()) # Clean variable
   memory.limit(150000)
-
+##### version info #####
+  # platform       x86_64-w64-mingw32          
+  # arch           x86_64                      
+  # os             mingw32                     
+  # system         x86_64, mingw32             
+  # status                                     
+  # major          4                           
+  # minor          1.1                         
+  # year           2021                        
+  # month          08                          
+  # day            10                          
+  # svn rev        80725                       
+  # language       R                           
+  # version.string R version 4.1.1 (2021-08-10)
+  # nickname       Kick Things
+  
 ##### 3.1 Term frequency in Jane Austen s novels #####
   library(dplyr)
   library(janeaustenr)
@@ -59,90 +74,12 @@
     scale_x_log10() +
     scale_y_log10()
 
-
-##### 3.3 The bind_tf_idf() function #####
-  book_tf_idf <- book_words %>%
-    bind_tf_idf(word, book, n)
-  
-  book_tf_idf
-  
-  book_tf_idf %>%
-    select(-total) %>%
-    arrange(desc(tf_idf))
-  
-  
-  library(forcats)
-  
-  book_tf_idf %>%
-    group_by(book) %>%
-    slice_max(tf_idf, n = 15) %>%
-    ungroup() %>%
-    ggplot(aes(tf_idf, fct_reorder(word, tf_idf), fill = book)) +
-    geom_col(show.legend = FALSE) +
-    facet_wrap(~book, ncol = 2, scales = "free") +
-    labs(x = "tf-idf", y = NULL)
-
 ##### 3.3 The BM25() function #####
-  # # https://rdrr.io/cran/superml/src/R/bm25.R
-  # bmscore = function(q, document_from_corpus, b = 0.75,k1 = 1.25){
-  #   
-  #   # # constant values
-  #   # b <- 0.75
-  #   # k1 <- 1.25
-  #   
-  #   freq_q <- sum(document == q)
-  #   doc_len <- length(document)
-  #   mean_doc_len <- mean(vapply(document, length, FUN.VALUE = integer(1)))
-  #   return(#private$calculate_idf(q, corpus) *
-  #            ((freq_q * (k1 + 1)) /
-  #               ((freq_q + k1) *
-  #                  (1 - b + b *
-  #                     (doc_len / mean_doc_len)))))
-  # }
-  
-  view(bind_tf_idf)
-  BM25Score = function(tbl, term, document, n, 
-                       b = 0.75, k1 = 1.25, sigmoid=1){
-    
-    term <- quo_name(enquo(term))
-    document <- quo_name(enquo(document))
-    n_col <- quo_name(enquo(n))
-    terms <- as.character(tbl[[term]])
-    documents <- as.character(tbl[[document]])
-    
-    doc_len <- length(documents)
-    mean_doc_len <- mean(vapply(documents, length, FUN.VALUE = integer(1)))
-    
-    n <- tbl[[n_col]]
-    doc_totals <- tapply(n, documents, sum)
-    idf <- log(length(doc_totals)/table(terms))
-    idf_BM25 <- log((length(doc_totals)-table(terms)+0.5)/(table(terms)+0.5))
-    tbl$tf <- n/as.numeric(doc_totals[documents])
-    tbl$idf <- as.numeric(idf[terms])
-    tbl$idf_BM25 <- as.numeric(idf_BM25[terms])
-    tbl$tf_idf <- tbl$tf * tbl$idf
-    tbl$bm25 <- (k1 + 1)*(tbl$tf) * tbl$idf_BM25/
-                (k1*(1 - b + b *(doc_len / mean_doc_len))+ tbl$tf)
-    if (sigmoid==1){
-    ## Sigmoid
-    # https://cran.r-project.org/web/packages/sigmoid/vignettes/sigmoid.html
-    library(sigmoid)
-    book_BM25_sig <- tbl
-    book_BM25_sig$bm25 <- sigmoid(tbl$bm25,method = c("ReLU"))
-    tbl <- book_BM25_sig
-    }
-    if (any(tbl$idf < 0, na.rm = TRUE)) {
-      rlang::warn(paste("A value for tf_idf is negative:\n", 
-                        "Input should have exactly one row per document-term combination."))
-    }
-    return(tbl)    
-  }
-  
-  
+  source("FUN_BM25.R")
   book_BM25 <- book_words %>%
                BM25Score(word, book, n)
 
-  plot(book_BM25_sig$tf_idf ,book_BM25_sig$bm25)
+  plot(book_BM25$tf_idf ,book_BM25$bm25)
   
   # ## Z-score
   # # https://www.r-bloggers.com/2020/02/how-to-compute-the-z-score-with-r/
